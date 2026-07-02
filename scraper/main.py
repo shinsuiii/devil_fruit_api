67d08801b3e40eb9624f4e5e946eb1f7aaeda20b
Shane <cunananshanejoey21@gmail.com>
2026-07-02 16:23:39 +0800

    working basic scraper

--- scraper/main.py
+++ scraper/main.py
@@ -1,7 +1,9 @@
-import requests
+import json
+import time
 from selectolax.lexbor import LexborHTMLParser
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
@@ -10,88 +12,64 @@ from selenium.webdriver.chrome.options import Options
 
 def main():
     driver = webdriver.Chrome()
-    urls = get_links("https://onepiece.fandom.com/wiki/Paramecia", driver)
-    print(urls)
 
-
-
-
-
-def get_links(root_url, driver):
-    driver.get(root_url)
-    table_row = WebDriverWait(driver, 10).until(
-        EC.presence_of_all_elements_located(By.CSS_SELECTOR, ".sortable.jquery-tablesorter")
+    root_url = "https://onepiece.fandom.com/wiki/Paramecia"
+    root_table = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.CSS_SELECTOR, ".sortable.jquery-tablesorter"))
     )
 
-    urls = []
-
-    for table in table_row:
-        urls.append(table.css_first("tbody tr td a").attributes.get("href"))
-
-    driver.quit()
-    return urls
-    ...
-
-
-def scrape():
-
-    ...
-
-
-
-
-
-
-
-
-def test_1():
-    url = "https://onepiece.fandom.com/wiki/Paramecia"
+    urls = get_links(driver, root_url, root_table)
     
-    driver = webdriver.Chrome()
-    driver.get(url)
+    data_list = []
 
-    cards = WebDriverWait(driver, 10).until(
-        EC.presence_of_all_elements_located(By.CSS_SELECTOR, "")
-    )
-
-    for card in cards:
-        data = {
-
-        }
-
-    driver.quit()
+    try:
+        driver.get(root_url)
 
 
-def test_2():
-    url = "https://onepiece.fandom.com/wiki/Paramecia"
+        main_table = root_table.find_elements(By.CSS_SELECTOR, "tbody tr")
 
-    driver = webdriver.Chrome()
-    driver.get(url)
+        for table in main_table:
+            data = {
+                "name": table.find_element(By.XPATH, ".//td[1]/a").text,
+                "user": table.find_element(By.XPATH, ".//td[2]/a").text,
+                "description": table.find_element(By.XPATH, ".//td[3]").text,
+            }
 
-    tables = driver.find_elements(By.CSS_SELECTOR, "")
+            data_list.append(data)
+            time.sleep(0.2)
 
-    # Searches only what is inside the container inside "tables" (tables == WebElement)
-    for table in tables:
-        name = table.find_element(By.CSS_SELECTOR, "")
-
-
-    print(driver.title)
+    except TimeoutException:
+        driver.quit()
+    
+    with open("data.json", "w") as file:
+        json.dump(data_list, file, indent=2)
 
     driver.quit()
 
 
 
-def get_img_src():
-    url = "https://onepiece.fandom.com/wiki/Paramecia"
-    driver = webdriver.Chrome()
-    driver.get(url)
+def get_links(driver, root_url, root_table):
+    try:
+        driver.get(root_url)
 
-    img = driver.find_element(By.TAG_NAME, "img")
+        
 
-    src = img.get_attribute("src")
-    print(src)
+        
 
-    driver.quit()
+
+
+
+    except TimeoutException:
+        driver.quit()
+
+
+
+
+
+
+
+
+
 
 
 
