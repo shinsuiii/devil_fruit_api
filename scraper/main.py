a3656d238dc63e43c2f7b2df4ee1181d6630efd3
Shane <cunananshanejoey21@gmail.com>
2026-07-03 21:46:11 +0800

    added canon and non-canon indicators for df

--- scraper/main.py
+++ scraper/main.py
@@ -12,61 +12,6 @@ from selenium.webdriver.support import expected_conditions as EC
 
 def main():
     root_url = "https://onepiece.fandom.com/wiki/Paramecia"
-    expanded_data = []
-    id_counter = 0
-
-    driver = webdriver.Chrome()
-
-    try:
-        driver.get(root_url)
-        root_table = WebDriverWait(driver, 10).until(
-            EC.visibility_of_all_elements_located((By.CSS_SELECTOR, ".sortable.jquery-tablesorter"))
-        )
-
-        for table in root_table:
-            target_row = table.find_elements(By.TAG_NAME, "tbody")
-
-            for row in target_row:
-                id_counter += 1
-
-                try:
-                    fruit_user = row.find_element(By.XPATH, ".//td[2]")
-                    span_indicator = fruit_user.find_element(By.CSS_SELECTOR, ".nomobile")
-                    # Checks the existence of a previous user.
-                    if span_indicator:
-                        previous_user = fruit_user.find_element(By.XPATH, ".//a[1]").text
-                        current_user = fruit_user.find_element(By.XPATH, ".//a[2]").text
-                    else:
-                        current_user = fruit_user.find_element(By.XPATH, ".//a[1]").text
-                except NoSuchElementException:
-                    pass
-
-                data = {
-                    "id": id_counter,
-                    "name": row.find_element(By.XPATH, ".//td[1]/a").text,
-                    "type": root_url.replace("https://onepiece.fandom.com/wiki/", ""),
-                    "current_user": current_user,
-                    **({"previous_user": previous_user}),
-                    "description": row.find_element(By.XPATH, ".//td[3]").text,
-                }
-
-                expanded_data.append(data)
-                time.sleep(0.1)
-            
-            id_counter = 0 # resets counter to zero after iterating through a tabl
-            time.sleep(0.1)
-        
-        with open("expanded_data.json", "w") as file:
-            json.dump(expanded_data, file, indent=2)
-    
-    except TimeoutException:
-        driver.quit()
-
-    driver.quit()
-
-
-def test():
-    root_url = "https://onepiece.fandom.com/wiki/Paramecia"
     driver = webdriver.Chrome()
     data_storage = []
     id_counter = 0
@@ -80,7 +25,12 @@ def test():
         for table in table_list:
             # Gets all tbody (9)
             target_row = table.find_elements(By.TAG_NAME, "tbody")
-            
+
+            # Determines if a df is canon or non-canon
+            reference_table = table_list[0]
+            before_ref = reference_table.find_element(By.XPATH, "./preceding-sibling::h3[1]/span[1]").text
+            after_ref = reference_table.find_element(By.XPATH, "./following-sibling::h3[1]/span[1]").text
+
             for row in target_row:
                 # Gets all tr inside a tbody
                 tr = row.find_elements(By.TAG_NAME, "tr")
@@ -89,18 +39,21 @@ def test():
                     id_counter += 1
                     data = {
                         "id": f"{id_counter}",
-                        "name": td.find_element(By.XPATH, ".//td[1]/a").text
+                        "name": td.find_element(By.XPATH, ".//td[1]/a").text,
+                        "type": "Paramecia",
+                        "canon_status": before_ref if table == reference_table else after_ref,
+                        "description": td.find_element(By.XPATH, ".//td[3]").text,
                     }
 
                     data_storage.append(data)
-                    time.sleep(0.1)
+                    time.sleep(0.2)
 
-            time.sleep(0.1)
+            time.sleep(0.2)
 
     except TimeoutException:
         driver.quit()
 
-    with open("expanded_data.json", "w") as file:
+    with open("data.json", "w") as file:
         json.dump(data_storage, file, indent=2)
 
     driver.quit()
@@ -108,5 +61,4 @@ def test():
 
 
 if __name__ == "__main__":
-    # main()
-    test()
+    main()
